0e323e29040618740c7c70dd94e0d31ccb7c5e5a
Larisse Alves <alves.larisser@gmail.com>
2020-01-05 16:32:21 -0300

    Modelagem dos App's

---
+++ comentario/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ComentavalConfig(AppConfig):
+    name = 'comentAval'
---
+++ pontosTuristicos/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from atracoes.models import Atracao
+from comentario.models import Comentario
+from avaliacoes.models import Avaliacao
+from localizacao.models import Localizacao
+
+class PontoTuristico (models.Model):
+    nome = models.CharField (max_length=150)
+    decricao = models.TextField ()
+    aprovado = models.BooleanField(default=False)
+    atracoes = models.ManyToManyField(Atracao)
+    comentarios = models.ManyToManyField(Comentario)
+    avaliacoes = models.ManyToManyField(Avaliacao)
+    localizacao = models.ForeignKey(Localizacao, on_delete=models.CASCADE)
+
+
+    def __str__ (self):
+        return self.nome
+
+
