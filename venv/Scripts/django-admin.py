5e67cc3a3c700411d620f6274ec9d44feb172439
Larisse Alves <alves.larisser@gmail.com>
2020-01-07 20:54:47 -0300

    Primeiro commit Heroku - API de pontos turisticos

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user\desktop\api-de-pontos-tur-sticos-\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
