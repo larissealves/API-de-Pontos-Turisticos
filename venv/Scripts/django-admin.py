d2a23b6366863931609a1e7fc658fd3f336e5afb
Larisse Alves <alves.larisser@gmail.com>
2020-01-08 12:24:49 -0300

    Confg do App

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\user\desktop\api-de-pontos-tur-sticos-\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
